82eb59e331bb5a74456f77be8e5ab0501a038500
Joe Connor <joe.connor@factorymedia.com>
2016-01-21 18:11:49 +0800

    add initial commit

---
+++ www/README.md
@@ -0,0 +1,28 @@
+# Mello Trello - Trello Board Printer
+
+Takes live data from Trello API and formats it for printing
+
+### How to run
+```
+bundle install
+cd www
+asdf
+```
+
+### How to access
+Open your browser at http://localhost:9292/index.html?boardId=[yourBoardId]
+
+
+### Problems? 
+
+You might well need to update the API key in `index.html` as per https://trello.com/app-key 
+
+### Can it be improved? 
+
+Yes, I'm sure it should be possible to do some much neater code and some proper wait/promises on the AJAX calls but for the sake of brevity this works
+Feel free to send me a pull request
+
+
+### Credits
+
+Credit to https://gist.github.com/mathiasrw/8710615 for the original gist
---
+++ www/javascripts/application.js
@@ -0,0 +1,123 @@
+(function(){
+
+  var data = {};
+  var boardId = getParameterByName('boardId');
+
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)")
+    var results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+
+  // Authorize Trello Account
+  function authorizeTrello(){
+    Trello.authorize({
+      type: 'popup',
+      name: 'Mello Trello',
+      scope: {
+        read: true,
+        write: false 
+      },
+      expiration: 'never',
+      success: function(){
+        console.log("Successful authentication");
+        fetchData();
+      },
+      error: function(){
+        console.log("Failed authentication"); 
+      }
+    });
+  }
+
+  // Fetch Data from Trello
+  function fetchData(){
+    Trello.get('boards/' + boardId, function(board){
+      var boardData = board;
+      Trello.get('boards/' + boardId + '/lists', function(lists){
+        boardData.lists = lists;
+        Trello.get('boards/' + boardId + '/cards', function(cards){
+          boardData.cards = cards;
+          showData(eatData(boardData));
+        })
+      });
+
+    })
+    
+  };
+      
+  // Munge data ready for use in template
+  function eatData(trelloJson){
+    var data = {
+      board: trelloJson.name,
+      lists: [],
+      ref: {}
+    }
+
+    for(i in trelloJson.lists){
+      var list = trelloJson.lists[i]
+      if(list.closed){
+        //continue
+      }
+      data.ref[list.id] = {
+        name: list.name,
+        cards: []
+      }
+      data.lists.push(data.ref[list.id])
+    }
+
+    for(i in trelloJson.cards){
+      var card = trelloJson.cards[i]
+      if(card.closed){
+        //continue
+      }
+        
+      data.ref[card.id] = {
+        name: card.name,
+        desc: marked(card.desc),
+        actions: []
+      }
+      
+      data.ref[card.idList].cards.push(data.ref[card.id])    
+    }
+
+    for(i in trelloJson.actions){
+      var action = trelloJson.actions[i]
+      if(action.type != "commentCard"){
+        continue
+      }
+         
+      data.ref[action.id] = {
+        text: marked(action.data.text),
+        date: moment(action.date).format('YYYY-MM-DD')
+      }
+      
+      try{
+        data.ref[action.data.card.id].actions.push(data.ref[action.id])
+      } catch(e) {
+        // Nothing!
+      }
+    }
+
+    return data;
+  }
+
+  // Convert data into Mustache template
+  function showData(data){
+    var template = $('#template-output').html()
+    console.log(JSON.stringify(data,null,2))
+    $('#out').html(Mustache.render(template, data))
+  }
+
+  // Run the code
+  function autorun() {
+    authorizeTrello();
+  }
+
+
+  // Run on DOM reay
+  $(function(){
+    autorun();
+  });
+
+})($)
